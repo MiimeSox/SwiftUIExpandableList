23c9a9ebbcc10d1de6caf28c7999ceff906863a9
MiimeSox <kontakt@malteschumacher.de>
2023-05-30 22:48:26 +0200

    Initial Commit

---
+++ SwiftUIExpandableList/SwiftUIExpandableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExpandableListApp.swift
+//  SwiftUIExpandableList
+//
+//  Created by Malte Schumacher on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExpandableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
